3f04ad59a6df9e7be2b88f23fb4c2ff792b3dbed
Berkan Serbes <berkan_serbes@hotmail.com>
2025-06-28 16:47:23 +0300

    Add JwtTokenHelper for JWT validation
    Introduces a new static class `JwtTokenHelper` in the
    `TradeWind.Shared.Helpers` namespace. This class includes
    the `ValidateJwtToken` method, which validates JSON Web
    Tokens (JWT) using parameters like `secretKey`, `issuer`,
    and `audience`. It returns a `ClaimsPrincipal` if the
    token is valid, or `null` if validation fails or an
    exception occurs. The implementation leverages
    `JwtSecurityTokenHandler` and `TokenValidationParameters`
    from the `Microsoft.IdentityModel.Tokens` namespace.

---
+++ backend/TradeWind/src/BuildingBlocks/TradeWind.Shared/Helpers/JwtTokenHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace TradeWind.Shared.Helpers;
+
+public static class JwtTokenHelper
+{
+	public static ClaimsPrincipal? ValidateJwtToken(string token, string? secretKey = "", string? issuer = "", string? audience = "")
+	{
+		if (string.IsNullOrWhiteSpace(secretKey) || string.IsNullOrWhiteSpace(issuer) || string.IsNullOrWhiteSpace(audience))
+			return null;
+		if (string.IsNullOrWhiteSpace(token))
+			return null;
+		try
+		{
+			var handler = new JwtSecurityTokenHandler();
+			var key = Encoding.UTF8.GetBytes(secretKey);
+
+			var tokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = new SymmetricSecurityKey(key),
+				ValidateIssuer = true,
+				ValidIssuer = issuer,
+				ValidateAudience = true,
+				ValidAudience = audience,
+				ValidateLifetime = true,
+				ClockSkew = TimeSpan.Zero
+			};
+
+			ClaimsPrincipal? principal = handler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+			if (validatedToken is not JwtSecurityToken jwtToken || !jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+			{
+				return null;
+			}
+
+			return principal;
+		}
+		catch (Exception)
+		{
+			return null;
+		}
+	}
+}
